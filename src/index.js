d4a07185699e3403809d0311a9e86b697a07ade6
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-10-06 10:22:29 +0100

    Update the README.md file by adding project description and personnel infos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import './style.css';
-import retrieveFromStorage from './modules/retrieveFromStorage.js';
 import displayTasks, { footer } from './modules/displayTasks.js';
 import addTask from './modules/addTask.js';
 
-const totalTasks = JSON.parse(retrieveFromStorage('todo')) || [];
+const totalTasks = [
+  {
+    description: 'Wash the dishes',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Food',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Baith',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Sleep',
+    completed: true,
+    index: 4,
+  },
+];
+
 const tasksListDisplay = document.querySelector('.display');
 
 if (totalTasks.length === 0) {
